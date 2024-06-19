341f69d56ba307794f4d3b54cd60ecba1af29f9e
Karishma271 <karishmapatel273@gmail.com>
2024-06-19 13:21:55 -0400

    add

---
+++ add.js
@@ -0,0 +1,84 @@
+// Documentation
+// https://firebase.google.com/docs/database/web/read-and-write
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  child,
+  get,
+  onValue,
+  set,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2XCuQRAbUxd4NF9rlMVwhS0qfZ8LWxCE",
+  authDomain: "workshop-5b8a6.firebaseapp.com",
+  databaseURL: "https://workshop-5b8a6-default-rtdb.firebaseio.com",
+  projectId: "workshop-5b8a6",
+  storageBucket: "workshop-5b8a6.appspot.com",
+  messagingSenderId: "223100028919",
+  appId: "1:223100028919:web:e82fa1f5988cba4e6510f2",
+  measurementId: "G-G22G29QC25"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase();
+
+// Fetch Comments
+const Comments = ref(database, "/Comments");
+
+// On data event
+onValue(
+  Comments,
+  (snapshot) => {
+    // Create a reference to the ul element
+    const ul = document.getElementById("messages");
+
+    // Empty the ul emelemt
+    ul.replaceChildren();
+
+    // Loop through Comments
+    snapshot.forEach((childSnapshot) => {
+      // Get key and children
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+
+      console.log(childKey);
+      console.log(childData);
+
+      // Add message to list
+      const text = document.createTextNode(
+        childData.message + " ~ " + childData.name
+      );
+      const li = document.createElement("li");
+      li.appendChild(text);
+      ul.appendChild(li);
+    });
+  },
+  {
+    onlyOnce: false,
+  }
+);
+
+// Add event to button
+const submit = document.getElementById("submit");
+const name = document.getElementById("name");
+const message = document.getElementById("message");
+
+submit.addEventListener("click", function () {
+  const Comments = ref(database, "Comments");
+  push(Comments, {
+    message: message.value,
+    name: name.value,
+    createdAt: new Date().getTime(),
+  });
+});
